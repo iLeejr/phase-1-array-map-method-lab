62f593ee2c974b72d1755a1ee1fec46b63bc84cd
Kaleb Malone <kalebmalone7@gmail.com>
2024-09-23 10:31:38 -0700

    compleed lab

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
+
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((tutorial) => {
+    return tutorial
+      .split(' ')
+      .map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+      .join(' ')
+  })
 }
+
+
